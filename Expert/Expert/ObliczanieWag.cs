dc4f398728620c4ab5037bc3861c16d5feb4c68a
trent <trent.blaugrana@gmail.com>
2017-10-03 12:31:59 +0200

    Poprawa działania mnożenia macierzy

--- Expert/Expert/ObliczanieWag.cs
+++ Expert/Expert/ObliczanieWag.cs
@@ -19,70 +19,69 @@ namespace Expert
 
         public static DataTable ustalMacierzWag(DataTable wagi)
         {
-            DataTable macierzWag = podniesMacierzDoKwardratu(wagi);
+            DataTable macierzWag = wagi.Copy();
+            DataTable macierzDoKwadratu = podniesMacierzDoKwardratu(wagi).Copy();
 
-            //foreach (DataColumn dc in wagi.Columns)
-            //{
-            //    decimal sumaKolumn = 0;
+            foreach (DataColumn dc in macierzDoKwadratu.Columns)
+            {
+                decimal sumaKolumn = 0;
 
-            //    foreach (DataRow dr in wagi.Rows)
-            //    {
-            //        if (dr.Table.Columns[dc.ColumnName].Ordinal > 0)
-            //        {
-            //            decimal wartoscWierszaKolumny = 0;
+                foreach (DataRow dr in macierzDoKwadratu.Rows)
+                {
+                    if (dr.Table.Columns[dc.ColumnName].Ordinal > 0)
+                    {
+                        decimal wartoscWierszaKolumny = 0;
 
-            //            bool czyWartoscPoprawna = decimal.TryParse(dr[dc].ToString(), out wartoscWierszaKolumny);
+                        bool czyWartoscPoprawna = decimal.TryParse(dr[dc].ToString(), out wartoscWierszaKolumny);
 
-            //            if (czyWartoscPoprawna)
-            //            {
-            //                sumaKolumn = sumaKolumn + wartoscWierszaKolumny;
-            //            }
-            //        }
-            //    }
+                        if (czyWartoscPoprawna)
+                        {
+                            sumaKolumn = sumaKolumn + wartoscWierszaKolumny;
+                        }
+                    }
+                }
 
-            //    foreach (DataRow dr in wagi.Rows)
-            //    {
-            //        if (dr.Table.Columns[dc.ColumnName].Ordinal > 0)
-            //        {
-            //            decimal wartoscKomorki = decimal.Parse(dr[dc].ToString());
-            //            decimal obliczonaWaga = decimal.Round(wartoscKomorki / sumaKolumn, ROUND);
-            //            macierzWag.Rows[dr.Table.Rows.IndexOf(dr)][dc] = obliczonaWaga;
-            //        }
-            //    }
-            //}
+                foreach (DataRow dr in macierzDoKwadratu.Rows)
+                {
+                    if (dr.Table.Columns[dc.ColumnName].Ordinal > 0)
+                    {
+                        decimal wartoscKomorki = decimal.Parse(dr[dc].ToString());
+                        decimal obliczonaWaga = decimal.Round(wartoscKomorki / sumaKolumn, ROUND);
+                        macierzWag.Rows[dr.Table.Rows.IndexOf(dr)][dc] = obliczonaWaga;
+                    }
+                }
+            }
 
-            //macierzWag.Columns.Add("Suma elementów");
-            //macierzWag.Columns.Add("Waga");
+            macierzWag.Columns.Add("Suma elementów");
+            macierzWag.Columns.Add("Waga");
 
-            //decimal sumaWszystkichKolumn = 0;
+            decimal sumaWszystkichKolumn = 0;
 
-            //foreach (DataRow dr in wagi.Rows)
-            //{
-            //    decimal sumaWierszy = 0;
+            foreach (DataRow dr in macierzDoKwadratu.Rows)
+            {
+                decimal sumaWierszy = 0;
 
-            //    foreach (DataColumn dc in wagi.Columns)
-            //    {
-            //        if (dr.Table.Columns[dc.ColumnName].Ordinal > 0 && dr.Table.Columns[dc.ColumnName].Ordinal < dr.Table.Columns.Count - 2)
-            //        {
-            //            sumaWierszy = sumaWierszy + decimal.Parse(dr[dc].ToString());
-            //        }
-            //    }
+                foreach (DataColumn dc in macierzDoKwadratu.Columns)
+                {
+                    if (dr.Table.Columns[dc.ColumnName].Ordinal > 0 && dr.Table.Columns[dc.ColumnName].Ordinal < dr.Table.Columns.Count - 2)
+                    {
+                        sumaWierszy = sumaWierszy + decimal.Parse(dr[dc].ToString());
+                    }
+                }
 
-            //    sumaWszystkichKolumn = sumaWszystkichKolumn + sumaWierszy;
+                sumaWszystkichKolumn = sumaWszystkichKolumn + sumaWierszy;
 
-            //    Console.WriteLine(sumaWierszy);
+                dr["Suma elementów"] = decimal.Round(sumaWierszy, ROUND);
+            }
 
-            //    dr["Suma elementów"] = decimal.Round(sumaWierszy, ROUND);
-            //}
+            Console.WriteLine("suma kolumn " + sumaWszystkichKolumn);
 
-            //Console.WriteLine("suma kolumn " + sumaWszystkichKolumn);
+            foreach (DataRow dr in macierzDoKwadratu.Rows)
+            {
+                decimal wagaWiersza = decimal.Parse(dr["Suma elementów"].ToString()) / sumaWszystkichKolumn;
 
-            //foreach (DataRow dr in wagi.Rows)
-            //{
-            //    decimal wagaWiersza = decimal.Parse(dr["Suma elementów"].ToString()) / sumaWszystkichKolumn;
-
-            //    dr["Waga"] = decimal.Round(wagaWiersza, ROUND);
-            //}
+                dr["Waga"] = decimal.Round(wagaWiersza, ROUND);
+            }
 
             return macierzWag;
         }
@@ -96,34 +95,42 @@ namespace Expert
 
         private static DataTable podniesMacierzDoKwardratu(DataTable macierz)
         {
-            DataTable macierzA = macierz;
-            DataTable macierzB = macierz;
+            DataTable macierzA = macierz.Copy();
 
-            DataTable macierzDoKwadratu = macierz;
+            DataTable macierzDoKwadratu = macierz.Copy();
 
-            int rozmiarMacierzy = macierzA.Rows.Count;
+            foreach (DataRow dr in macierzDoKwadratu.Rows)
+            {
+                foreach (DataColumn dc in macierzDoKwadratu.Columns)
+                {
+                    if (dc.Table.Columns[dc.ColumnName].Ordinal > 0)
+                    {
+                        dr[dc] = 0;
+                    }
+                }
+            }
+
+            int rozmiarMacierzy = macierz.Rows.Count;
 
             for (int i = 0; i < rozmiarMacierzy; i++)
             {
-                for (int j = 0; j < rozmiarMacierzy; j++)
+                for (int j = 1; j <= rozmiarMacierzy; j++)
                 {
-                    macierzDoKwadratu.Rows[i][j] = 0;
+                    double val = 0;
+
+                    int kol = 1;
 
                     for (int k = 0; k < rozmiarMacierzy; k++)
                     {
-                        if (k == 0)
-                        {
-                            macierzDoKwadratu.Rows[i][0] = macierzA.Rows[i][0];
-                            macierzDoKwadratu.Rows[0][j] = macierzA.Rows[0][j];
-                        }
-                        else
-                        {
-                            double valueA = Convert.ToDouble(macierzA.Rows[i][k].ToString());
-                            double valueB = Convert.ToDouble(macierzB.Rows[k][j].ToString());
-                            double value = valueA * valueB;
-                            macierzDoKwadratu.Rows[i][j] = value;
-                        }
+                        double valueA = Convert.ToDouble(macierzA.Rows[i][kol].ToString());
+                        double valueB = Convert.ToDouble(macierzA.Rows[k][j].ToString());
+                        double value = valueA * valueB;
+                        val += value;
+
+                        kol++;
                     }
+
+                    macierzDoKwadratu.Rows[i][j] = val;
                 }
             }
 

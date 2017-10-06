91d42bcbf1e0ca41c494c44741313a073ef52a24
trent <trent.blaugrana@gmail.com>
2017-10-06 14:39:24 +0200

    Poprawki w przechodzeniu przez panele

--- Expert/Expert/Views/KryteriumPanel.cs
+++ Expert/Expert/Views/KryteriumPanel.cs
@@ -38,7 +38,7 @@ namespace Expert
             this.buttonMenu = buttonMenu;
             buttonMenu.setKryteriumPanel(this);
             setButtonEnable("Dalej", false);
-            this.Visible = true;
+            Visible = true;
         }
 
         private void celRadioButton_CheckedChanged(object sender, EventArgs e)
@@ -121,9 +121,10 @@ namespace Expert
                 {
                     Visible = false;
                     WagiPanel wagiPanel = new WagiPanel(mainForm, buttonMenu, celID);
+                    buttonMenu.setWagiPanel(wagiPanel);
+                    buttonMenu.setAktualnyPanel(wagiPanel);
                     mainForm.Controls.Add(wagiPanel);
                     wagiPanel.Visible = true;
-                    buttonMenu.setAktualnyPanel(wagiPanel);
                 }
                 else
                 {
